4d0756e0e9a004985f3483820235c3720ebb9c65
Ricardo Rubio <rickarubio@gmail.com>
2013-07-30 09:34:53 -0600

    add review about hashes

---
+++ hashes_review.rb
@@ -0,0 +1,48 @@
+# Quick Refresher on Hashes
+# http://rubylearning.com/satishtalim/ruby_hashes.html
+
+# Hashes are indexed collections of object references
+# Arrays are indexed with integers
+# Hashes can be indexed with objects of any type
+
+# To store a value in a Hash, you supply two objects
+# the index (called the key) and the value
+
+# You can retrieve the value by indexing the hash with the same key
+# The values in a hash can be objects of any type
+
+# This array holds 3 object references, indexed as 0, 1, 2
+an_array = [1,2,3]
+# Accessing index 0 of the array an_array
+puts an_array[0] # => 1
+
+# Now for Hashes...
+# Hash literals
+# key => value pair
+a_hash = {'cat' => 'kitteh von winklestein', 'dog' => 3.14}
+puts " hash length is #{a_hash.length}"
+puts a_hash['cat']
+puts a_hash['dog']
+
+# The advantage of using a hash over an array is that you can use
+# Any object as an index
+
+# If you try access a key that does not exist in the hash, the default value is nil
+
+# Returns an array of every Symbol used in my program
+puts Symbol.all_symbols.inspect
+
+# Instead of Strings, use Symbols for Hash keys (indexes), as they are more efficient
+people = Hash.new
+people2 = {:cat => 'mr.nachos', :dog => 'barky'}
+# Notice there are the two difference ways of assigning keys to Hashes
+# Either during first initialization of the Hash, or after once the Hash has been made.
+people[:nickname] = 'Rick'
+people[:lastname] = 'Rubio'
+
+p people
+p people2
+
+# Instead of using Hashrocket => you can use : but the Hashrocket is the Ruby preferred way
+people3 = {store: 'Ikea', color: 'maroon', price: 'inexpensive'} # note that this will not work with a numeric key
+p people3
--- level1followalong.rb
+++ level1followalong.rb
@@ -10,3 +10,117 @@ unless tweets.empty?
   puts "Timeline:"
   puts tweets
 end
+
+# Using unless with else is confusing, and should be avoided:
+unless tweets.empty?
+  puts "Timeline:"
+  puts tweets
+else
+  puts "No tweets found - better follow some people!"
+end
+
+# If you're going to using the else, then use this form:
+if tweets.empty?
+  puts "No tweets found - better follow some people!"
+else
+  puts "Timeline:"
+  puts tweets
+end
+
+# NIL is false in Ruby
+# Instead of writing code like this:
+if attachment.file_path != nil
+  attachment.post
+end
+
+# Write this instead:
+if attachment.file_path
+  attachment.post
+end
+
+# Not everything you think is nil will evaluate to nil (false):
+# "" => true
+# 0 => true
+# [] => true
+# Remember in Ruby only false and nil are, well, false and nil.
+
+# Do no write code like this in Ruby:
+unless name.length # this will never be false
+  warn "User name required"
+end
+
+# instead of writing these couple lines:
+if password.length < 8
+  fail "Password too short"
+end
+unless username # read as: unless "this is true" do this
+  fail "No user name set"
+end
+
+# Use this form instead: (inline conditionals)
+fail "Password too short" if password.length < 8
+fail "No user name set" unless username
+
+# Short Circuit "AND" (if the first result is false, don't bother checking the 2nd)
+if user
+  if user.signed_in?
+    # do stuff
+  end
+end
+
+# Here is the better way to write the above code:
+# if user is nil, second half never runs
+if user && user.signed_in?
+  # do stuff
+end
+
+# short circuit assignment
+result = nil || 1 # => 1 (because nil is falsy)
+result = 1 || nil # => 1
+# the second part of this is never checked because the first thing is true
+result = 1 || 2 # => 1
+
+# Default Values "OR"
+tweets = timeline.tweets
+tweets = [] unless tweets # if nothing got returned from those tweets, set to empty array
+
+# A better way:
+# if nil, default to empty array
+tweets = timeline.tweets || []
+
+# Short Circuit Evaluation
+def sign_in
+  current_session || sign_user_in # if current session is nil, then call sign_user_in
+end
+
+# Conditional Assignment, Good way to set defaults
+i_was_set = 1
+i_was_set ||= 2
+puts i_was_set # 1
+
+i_was_not_set ||= 2 # nil || 2
+puts i _was_not_set # 2
+
+# Refactor this Code:
+options[:country] = 'us' if options[:country].nil?
+options[:privacy] = true if options[:privacy].nil?
+options[:geotag] = true if options[:geotag].nil?
+# use conditional assignment operator
+options[:country] ||= 'us'
+options[:privacy] ||= true
+options[:geotag] ||= true
+
+# Conditional Return Values
+
+
+
+
+
+
+
+
+
+
+
+
+
